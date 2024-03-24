e7a9f2b0857834833222e3d2f89c73349ededb55
Reshzera <reoshiro13@gmail.com>
2024-03-24 11:13:12 -0300

    feat: add oppenzeplin ntf

--- contracts/ProtoNTF.sol
+++ contracts/ProtoNTF.sol
@@ -1,9 +1,61 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
-// Uncomment this line to use console.log
-// import "hardhat/console.sol";
+contract ProtoNTF is
+    ERC721,
+    ERC721Enumerable,
+    ERC721Burnable,
+    ERC721URIStorage
+{
+    uint256 private _tokenIdCounter;
 
-contract ProtoNTF{
+    constructor() ERC721("ProtoNTF", "PNTF") {}
 
+    function _baseURI() internal pure override returns (string memory) {
+        return "https://gateway.pinata.cloud/ipfs/QmZ/";
+    }
+
+    function mint() public {
+        _tokenIdCounter++;
+        _safeMint(msg.sender, _tokenIdCounter);
+        _setTokenURI(_tokenIdCounter, Strings.toString(_tokenIdCounter));
+    }
+
+    function _increaseBalance(
+        address account,
+        uint128 value
+    ) internal override(ERC721, ERC721Enumerable) {
+        super._increaseBalance(account, value);
+    }
+
+    function tokenURI(
+        uint256 tokenId
+    ) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+        return string.concat(super.tokenURI(tokenId), ".json");
+    }
+
+    function _update(
+        address to,
+        uint256 tokenId,
+        address auth
+    ) internal override(ERC721, ERC721Enumerable) returns (address) {
+        return super._update(to, tokenId, auth);
+    }
+
+    function supportsInterface(
+        bytes4 interfaceId
+    )
+        public
+        view
+        override(ERC721, ERC721Enumerable, ERC721URIStorage)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
 }
--- test/ProtoNTF.ts
+++ test/ProtoNTF.ts
@@ -1,10 +1,12 @@
+import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
+import { expect } from "chai";
 import hre from "hardhat";
 
 describe("ProtoNTF", function () {
   // We define a fixture to reuse the same setup in every test.
   // We use loadFixture to run this setup once, snapshot that state,
   // and reset Hardhat Network to that snapshot in every test.
-  async function deployOneYearLockFixture() {
+  async function deployFixture() {
     // Contracts are deployed using the first signer/account by default
     const [owner, otherAccount] = await hre.ethers.getSigners();
 
@@ -13,4 +15,41 @@ describe("ProtoNTF", function () {
 
     return { protoNTF, owner, otherAccount };
   }
+
+  it("Should has name and symbol", async function () {
+    const { protoNTF } = await loadFixture(deployFixture);
+
+    expect(await protoNTF.name()).to.equal("ProtoNTF");
+    expect(await protoNTF.symbol()).to.equal("PNTF");
+  });
+
+  it("Should mint a token", async function () {
+    const { protoNTF, owner } = await loadFixture(deployFixture);
+
+    await protoNTF.mint();
+
+    const ownerBalance = await protoNTF.balanceOf(owner.address);
+    const ownerOfToken = await protoNTF.ownerOf(1);
+    const totalSupply = await protoNTF.totalSupply();
+
+    expect(ownerBalance).to.equal(1);
+    expect(ownerOfToken).to.equal(owner.address);
+    expect(totalSupply).to.equal(1);
+  });
+
+  it("Should has URI metadata", async function () {
+    const { protoNTF } = await loadFixture(deployFixture);
+
+    await protoNTF.mint();
+
+    const tokenURI = await protoNTF.tokenURI(1);
+
+    expect(tokenURI).to.equal("https://gateway.pinata.cloud/ipfs/QmZ/1.json");
+  });
+
+  it("Should support interface", async function () {
+    const { protoNTF } = await loadFixture(deployFixture);
+
+    expect(await protoNTF.supportsInterface("0x80ac58cd")).to.equal(true);
+  });
 });
